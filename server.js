8825ee7d1967b14bcee029e9caca1ec6666d2636
Bhoomi Kaushik <kaushikbhumika13@gmail.com>
2026-08-07 12:44:54 +0530

    Trying to commit

--- server.js
+++ server.js
@@ -3,10 +3,7 @@ dotenv.config()
 const app = require("./src/app")
 
 
-
-
-
 const PORT=process.env.PORT
 app.listen(PORT ,()=>{
-    console.log(`🚀Server is running at ${PORT}`)
+    console.log(`👋Welcome to the Backend World 🚀Server is running at ${PORT}`)
 })
