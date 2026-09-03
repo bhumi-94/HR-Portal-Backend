e68897841db60fb0b70b2f73d5f9523f01896df0
Bhoomi Kaushik <kaushikbhumika13@gmail.com>
2026-09-03 18:21:32 +0530

    Prepare backend for deployment

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ const app = require("./src/app")
 require("./Jobs/holidayNotification.job");
 
 const PORT=process.env.PORT
-app.listen(PORT ,()=>{
+app.listen(PORT , "0.0.0.0" ,() => {
     console.log(`👋Welcome to the Backend World 🚀Server is running at ${PORT}`)
 })
